ce490edbcaf76d207696c07d2c55837189aed5d2
Zen <zhengxu5511@gmail.com>
2018-04-22 23:34:42 +0100

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :fake_apis
   # resources :apis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
