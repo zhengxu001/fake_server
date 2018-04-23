ff3dc3ed7d089566ba6cea7379d22c9195bb898a
Zen <zhengxu5511@gmail.com>
2018-04-23 12:00:46 +0100

    add signin/signup function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :fake_apis
+  root to: "fake_apis#index"
   match "*path", to: "fake_apis#fake_api", via: :all
 end
